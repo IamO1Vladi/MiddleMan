f267c0cb158ec10c666c4fa90dd9b02f617c061c
IamO1Vladi <vladimir96930@gmail.com>
2024-10-10 18:48:25 +0300

    Added information page

---
+++ MiddleMan/Controllers/InformationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MiddleMan.Controllers
+{
+    public class InformationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
