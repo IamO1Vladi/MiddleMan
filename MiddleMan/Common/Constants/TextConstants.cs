a9c0fd8b96295cccc16f438fc8bc7606f772ae77
IamO1Vladi <vladimir96930@gmail.com>
2024-10-12 01:11:52 +0300

    Added HomePage text constants

--- MiddleMan/Common/Constants/TextConstants.cs
+++ MiddleMan/Common/Constants/TextConstants.cs
@@ -5,7 +5,7 @@ public static class TextConstants
 
     //TopBarNavigation
 
-    public const string OpenHoursText = "Open Hours: Mon - Sat - 9:00 - 18:00";
+    public const string OpenHoursText = "Always available";
     public const string PhoneNumber = "+359892456245";
     public const string EmailAddress = "vladimir96930@gmail.com";
     public const string FaceBookLink = "https://facebook.com";
@@ -17,12 +17,81 @@ public static class TextConstants
     public const string HomeButtonName ="Home";
     public const string AboutButtonName = "About";
     public const string ServicesButtonName = "Services";
-    public const string BlogButtonName = "Blog";
+    public const string InformationButtonName = "Information";
     public const string GetInTouchButtonName = "Get in touch";
     public const string FirstPartLogoName = "Sino";
     public const string SecondPartLogoName = "Xpert";
 
 
+    //HomePage
+
+    public const string SubHeaderMessage = "Thank you for stopping by!";
+    public const string HeaderMessage = "Your Connection for Business Success in China";
+
+    //HomePage -> About Company Section
+    public const string SubHeaderMessageAboutCompanySection = "About our network";
+    public const string CompanySectionHeaderMessage = "SinoXpert";
+
+    public const string CompanySectionDescription =
+        "SinoXpert specializes in connecting businesses and professionals with proven suppliers in China. Our unique services are designed to offer tailored solutions for your business needs by conducting in-depth research, quality control, and facilitating seamless communication.";
+
+    public const string CompanyValueOne = "Transparency";
+    public const string CompanyValueOneSummary = "Keeping You Informed Every Step of the Way";
+
+    public const string CompanyValueTwoHeader = "Quality";
+    public const string CompanyValueTwoSummary =
+        "Ensuring Precision, Striving for Perfection, and Delivering with Punctuality";
+
+    public const string CompanyValueThreeHeader = "Flexibility";
+    public const string CompanyValueThreeSummary = "Adapting to Your Unique Needs and Challenges";
+
+    public const string CompanyValueFourHeader = "Expertise";
+    public const string CompanyValueFourSummary = "A Unified Team Aligned with Your Business Needs";
+
+    //Homepage -> About Services
+    public const string ServiceSectionHeader = "Excellence in Services";
+
+    public const string ServiceSectionDescription =
+        "At SinoXpert, our services are tailored to your specific business needs. We believe flexibility and agility are crucial in today’s fast-changing business environment. That’s why our solutions are designed to deliver exactly what you need, quickly and efficiently.";
+
+    public const string ServiceOneHeader="Find a supplier";
+    public const string ServiceOneSummary = "Find a Supplier: Give Us Your Specs, and We’ll Source the Best";
+
+    public const string ServiceTwoHeader = "Organise a factory tour";
+    public const string ServiceTwoSummary = "See Supplier’s Operations Up Close and choose the best";
+
+    public const string ServiceThreeHeader = "Talen development";
+    public const string ServiceThreeSummary = "Unlock Educational and Career Opportunities in China";
+
+    public const string ServiceFourHeader = "Expand your business in China";
+    public const string ServiceFourSummary = "We assist your company in every step of entering the Chinese market";
+
+    //Homepage -> Company WorkFlow section
+    public const string WorkflowSectionHeader = "How do we do it";
+
+    public const string WorkflowSectionDescription =
+        "From Concept to Completion: We guide you through every stage, ensuring smooth execution and delivering results that meet your expectations.";
+
+    public const string WorkflowStepOneHeader = "Understanding your needs";
+
+    public const string WorkflowStepOneSummary =
+        "Initial consultation Define clear objectives Establishing time benchmarks";
+
+    public const string WorkflowStepTwoHeader = "Developiing a Customised Strategy";
+    public const string WorkflowStepTwoSummary = "Developing Strategies That Fit Your Business Objectives";
+
+    public const string WorkflowStepThreeHeader = "Establishing communication";
+    public const string WorkflowStepThreeSummary = "Establishing communication with suppliers";
+
+    public const string WorkflowStepFourHeader = "Quality Control and Monitoring";
+    public const string WorkflowStepFourSummary = "Ensuring Excellence with Continuous Quality Control and Monitoring";
+
+    public const string WorkflowStepFiveHeader = "Bringing It All Together";
+    public const string WorkflowStepFiveSummary = "Choosing the Ideal Supplier with a Focus on Quality and Value";
 
 
+    //HomePage -> Information section
+
+    public const string InformationSectionHeader = "Information";
+    public const string InformationSectionDescription = "All the Information You Need and More!";
 }
